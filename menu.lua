d47ddf2b5217f13fd4a6f2803f19dd97510757ad
Christ8818 <Christ8818@hotmail.it>
2019-11-24 19:36:54 +0100

    funzioni listener e displayGroups
    aggiunte le funzioni per aggiungere e rimuovere listener di determinati gruppi.
    aggiunti i gruppi necessari

--- menu.lua
+++ menu.lua
@@ -6,10 +6,39 @@ local scene = composer.newScene()
 -- Code outside of the scene event functions below will only be executed ONCE unless
 -- the scene is removed entirely (not recycled) via "composer.removeScene()"
 -- -----------------------------------------------------------------------------------
-local function gotoGame()
+local function gotoCannone()
     composer.gotoScene( "cannone" , { time=800, effect="crossFade" } )
 end
- 
+
+local function gotoGiocoliere()
+    composer.gotoScene( "giocoliere" , { time=800, effect="crossFade" } )
+end
+
+local function aggiungiGiochiListener()
+    cannoneButton:addEventListener( "tap", gotoCannone )
+    giocoliereButton:addEventListener( "tap", gotoGiocoliere )
+end
+
+local function rimuoviGiochiListener()
+    cannoneButton:removeEventListener( "tap", gotoCannone )
+    giocoliereButton:RemoveEventListener( "tap", gotoGiocoliere )
+end
+
+local function aggiungiprincipaleListener()
+    playButton:addEventListener( "tap", gotoGiochi )
+end
+
+local function rimuoviPrincipaleListener()
+    playButton:removeEventListener( "tap", gotoGiochi )
+end
+
+local function gotoGiochi()
+    transition.to( principaleGroup, { time=500, effect="crossFade",
+                                      onStart=rimuoviPrincipaleListener,
+                                      onComplete=aggiungiGiochiListener} )
+end
+
+
 -- -----------------------------------------------------------------------------------
 -- Scene event functions
 -- -----------------------------------------------------------------------------------
@@ -19,16 +48,42 @@ function scene:create( event )
 
 	local sceneGroup = self.view
 	-- Code here runs when the scene is first created but has not yet appeared on screen
-	local background = display.newImageRect( sceneGroup, "images/sfondo1.png", 3000, 1280 )
-    background.x = display.contentCenterX
+
+  -- setup display groups
+
+  local backGroup = display.newGroup()
+  sceneGroup:insert( backGroup )
+
+  local impostazioniGroup = display.newGroup()
+  sceneGroup:insert( impostazioniGroup )
+
+  local punteggiGroup = display.newGroup()
+  sceneGroup:insert( punteggiGroup )
+
+  local giochiGroup = display.newGroup()
+  sceneGroup:insert( giochiGroup )
+
+  local principaleGroup = display.newGroup()
+  sceneGroup:insert( principaleGroup )
+
+	local background = display.newImageRect( backGroup, "images/sfondo1.png", 3000, 1280 )
+  background.x = display.contentCenterX
 	background.y = display.contentCenterY
-	
-    local title = display.newText( sceneGroup, "Freak Circus", display.contentCenterX, 300, native.systemFont, 200 );
-    
-	local playButton = display.newText( sceneGroup, "Gioca", display.contentCenterX, 1100, native.systemFont, 150 )
-    playButton:setFillColor( 1, 1, 1 )
- 	
-	playButton:addEventListener( "tap", gotoGame )
+
+  local title = display.newText( backGroup, "Freak Circus", display.contentCenterX, 300, native.systemFont, 200 );
+
+  -- bottoni principaleGroup
+  local playButton = display.newText( principaleGroup, "Gioca", display.contentCenterX, 1000, native.systemFont, 100 )
+      playButton:setFillColor( 1, 1, 1 )
+
+  -- bottoni giochiGroup
+	local cannoneButton = display.newText( giochiGroup, "Cannone", display.contentCenterX, 1050, native.systemFont, 100 )
+      cannoneButton:setFillColor( 1, 1, 1 )
+
+  local giocoliereButton = display.newText( giochiGroup, "Giocoliere", display.contentCenterX, 1150, native.systemFont, 100 )
+      giocoliereButton:setFillColor( 1, 1, 1 )
+
+  playButton:addEventListener( "tap", gotoGiochi )
 end
 
 

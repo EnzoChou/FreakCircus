eb1cfb0540d96ab13fd6a0a3558e9264fbf0a3e7
Christ8818 <Christ8818@hotmail.it>
2019-11-27 18:12:29 +0100

    spostamento scene menu implementato
    diviso il menu in compartimenti e aggiunto il tasto indietro.

--- menu.lua
+++ menu.lua
@@ -6,6 +6,29 @@ local scene = composer.newScene()
 -- Code outside of the scene event functions below will only be executed ONCE unless
 -- the scene is removed entirely (not recycled) via "composer.removeScene()"
 -- -----------------------------------------------------------------------------------
+
+-- initialize local variables
+local backGroup
+local impostazioniGroup
+local punteggiGroup
+local giochiGroup
+local principaleGroup
+local gruppoInScena
+
+local background
+local title
+
+-- bottoni principaleGroup
+local playButton
+local impostazioniButton
+
+-- bottoni giochiGroup
+local cannoneButton
+local giocoliereButton
+
+-- bottone indietro
+local indietroButton
+
 local function gotoCannone()
     composer.gotoScene( "cannone" , { time=800, effect="crossFade" } )
 end
@@ -32,10 +55,29 @@ local function rimuoviPrincipaleListener()
     playButton:removeEventListener( "tap", gotoGiochi )
 end
 
+local function giochiInScena()
+    transition.to( giochiGroup, { time = 1000, transition = easing.inOutElastic,
+                                  x=0 } )
+    gruppoInScena=giochiGroup
+end
+
 local function gotoGiochi()
-    transition.to( principaleGroup, { time=500, effect="crossFade",
-                                      onStart=rimuoviPrincipaleListener,
-                                      onComplete=aggiungiGiochiListener} )
+    transition.to( principaleGroup, { time = 1000, transition = easing.inOutElastic,
+                                      x = -3000,
+                                      onStart=giochiInScena } )
+end
+
+local function menuPrincipale()
+    transition.to( principaleGroup, { time = 1000, transition = easing.inOutElastic,
+                                      x = 0 } )
+end
+
+local function tornaMenuPrincipale()
+    if ( gruppoInScena ~= principaleGroup ) then
+        transition.to( gruppoInScena, { time = 1000, transition = easing.inOutElastic,
+                                    x = 3000,
+                                    onStart = menuPrincipale } )
+    end
 end
 
 
@@ -51,40 +93,52 @@ function scene:create( event )
 
   -- setup display groups
 
-  local backGroup = display.newGroup()
+  backGroup = display.newGroup()
   sceneGroup:insert( backGroup )
 
-  local impostazioniGroup = display.newGroup()
+  impostazioniGroup = display.newGroup()
   sceneGroup:insert( impostazioniGroup )
+  impostazioniGroup.x = 3000
 
-  local punteggiGroup = display.newGroup()
+  punteggiGroup = display.newGroup()
   sceneGroup:insert( punteggiGroup )
+  punteggiGroup.x = 3000
 
-  local giochiGroup = display.newGroup()
+  giochiGroup = display.newGroup()
   sceneGroup:insert( giochiGroup )
+  giochiGroup.x = 3000
 
-  local principaleGroup = display.newGroup()
+  principaleGroup = display.newGroup()
   sceneGroup:insert( principaleGroup )
 
-	local background = display.newImageRect( backGroup, "images/sfondo1.png", 3000, 1280 )
+	background = display.newImageRect( backGroup, "images/sfondo1.png", 3000, 1280 )
   background.x = display.contentCenterX
 	background.y = display.contentCenterY
 
-  local title = display.newText( backGroup, "Freak Circus", display.contentCenterX, 300, native.systemFont, 200 );
+  title = display.newText( backGroup, "Freak Circus", display.contentCenterX, 300, native.systemFont, 200 );
 
   -- bottoni principaleGroup
-  local playButton = display.newText( principaleGroup, "Gioca", display.contentCenterX, 1000, native.systemFont, 100 )
+  playButton = display.newText( principaleGroup, "Gioca", display.contentCenterX, 1000, native.systemFont, 100 )
+      playButton:setFillColor( 1, 1, 1 )
+
+  impostazioniButton = display.newText( principaleGroup, "Impostazioni", display.contentCenterX, 1150, native.systemFont, 100 )
       playButton:setFillColor( 1, 1, 1 )
 
   -- bottoni giochiGroup
-	local cannoneButton = display.newText( giochiGroup, "Cannone", display.contentCenterX, 1050, native.systemFont, 100 )
+	cannoneButton = display.newText( giochiGroup, "Cannone", display.contentCenterX, 1000, native.systemFont, 100 )
       cannoneButton:setFillColor( 1, 1, 1 )
 
-  local giocoliereButton = display.newText( giochiGroup, "Giocoliere", display.contentCenterX, 1150, native.systemFont, 100 )
+  giocoliereButton = display.newText( giochiGroup, "Giocoliere", display.contentCenterX, 1150, native.systemFont, 100 )
       giocoliereButton:setFillColor( 1, 1, 1 )
+
+  -- bottone indietro al menu principale
+  indietroButton = display.newText( backGroup, "Indietro", -500, 150, native.systemFont, 100 )
+      indietroButton:setFillColor( 1, 1, 1 )
 
   playButton:addEventListener( "tap", gotoGiochi )
   cannoneButton:addEventListener( "tap", gotoCannone )
+  giocoliereButton:addEventListener( "tap", gotoGiocoliere )
+  indietroButton:addEventListener( "tap", tornaMenuPrincipale )
 end
 
 
